aee325c755f31077f7cd0264dce15779cf427783
FaishallRr <faishalrasyid4@gmail.com>
2025-10-19 18:32:43 +0700

    perubahana firebase-admin

--- BackEnd/src/firebase/firebase-admin.ts
+++ BackEnd/src/firebase/firebase-admin.ts
@@ -1,9 +1,11 @@
 import * as admin from 'firebase-admin';
 import { ServiceAccount } from 'firebase-admin';
-import serviceAccount from '../../serviceAccountKey.json';
+import serviceAccountJson from '../../serviceAccountKey.json';
+
+const serviceAccount = serviceAccountJson as ServiceAccount;
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as ServiceAccount),
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://portofolio-6b4fb-default-rtdb.firebaseio.com/',
 });
 
